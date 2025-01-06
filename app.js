9e8946bb6f107a014e66402646559f6d7cb7e0ac
git <gubbalasantoshkumar2004@gmail.com>
2025-01-06 10:04:58 +0530

    (password hash) bcrypt

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const session = require('express-session');
 const LocalStrategy = require('passport-local');
 
 const app = express();
+
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
 app.use(bodyParser.json());
+
 app.use(express.urlencoded({extended:false}));
 
 app.use(session({
@@ -86,17 +92,17 @@ app.get('/signup',(req,res)=>{
 })
 
 app.post('/users',async (req,res)=>{
-    console.log("FirstName :",req.body);
+    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
+    console.log(hashedPwd);
     try{
         const user = await User.create({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             email: req.body.email,
-            password: req.body.password,
-    
+            password: hashedPwd
         })
         req.login(user,(err)=>{
-            if(err){
+            if (err){
                 console.log(err);
             }
             res.redirect('/todos');
@@ -105,7 +111,79 @@ app.post('/users',async (req,res)=>{
     catch(err){
         console.log(err);
     }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, async (username, password, done) => {
+    try {
+        const user = await User.findOne({ where: { email: username } });
+        if (!user) {
+            return done(null, false, { message: 'Invalid email or password' });
+        }
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            return done(null, false, { message: 'Invalid email or password' });
+        }
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    console.log("Serializing user in session", user.id)
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.findByPk(id)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch((error) => {
+            done(error, null);
+        });
+});
+
+app.set("view engine", "ejs");
+
+app.get('/', async (req, res) => {
+    res.render('index', {
+        title: "Todo application",
+    });
+});
+
+app.get('/todos', connectEnsurelogin.ensureLoggedIn(), async (req, res) => {
+    const allTodos = await Todo.getTodos();
+    const today = new Date().toISOString().split('T')[0];
+    const overdue = allTodos.filter(todo => todo.dueDate < today);
+    const dueToday = allTodos.filter(todo => todo.dueDate === today);
+    const dueLater = allTodos.filter(todo => todo.dueDate > today);
+
+    if (req.accepts("html")) {
+        res.render('todos', {
+            allTodos,
+            overdue,
+            dueToday,
+            dueLater
+        });
+    } else {
+        res.json({ allTodos });
+    }
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup', { title: "Signup" });
 })
+
+app.post('/users', async (req, res) => {
+    
+});
 
 app.get('/login',(req,res)=>{
     res.render('login');
